640c69322452dafe27fb91b84a0ceee1cca3eb74
Scott Carpenter <scottcarpenter01@gmail.com>
2021-08-08 20:28:28 +0100

    Adds jest configuration (enables yarn test) Also adds a route that makes the /users/{userId}/transactions route return http 200

---
+++ src/routers/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+
+const usersRouter = express.Router();
+
+
+usersRouter.get("/:userId/transactions", function(req, res) {
+    console.log(req.params.userId);
+    res.json({success: true});
+});
+
+module.exports = usersRouter;
--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ const app = express();
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const userRouter = require("./routers/users"); 
 
+app.use('/users', userRouter);
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
