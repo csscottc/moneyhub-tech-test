b8b68091ac3044eae86098ffb826cb0ee38fbee2
Scott Carpenter <scottcarpenter01@gmail.com>
2021-08-08 19:56:55 +0100

    Adds a (failing) unit test and some basic jest configuration

---
+++ src/__test__/server.test.js
@@ -0,0 +1,9 @@
+const { app } = require("../server");
+
+describe("API", () => {
+    describe("/docs", () => {
+        it("Returns a HTTP 200 response", () => {
+            expect(true).toBe(false);
+        });
+    });
+});
